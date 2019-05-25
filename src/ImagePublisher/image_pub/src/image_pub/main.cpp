7777a2d05cda50d3e2a3d295ecbabeeb7557fdf9
juancki <jcgp97@gmail.com>
2019-05-25 22:08:54 +0200

    Video output

--- src/ImagePublisher/image_pub/src/image_pub/main.cpp
+++ src/ImagePublisher/image_pub/src/image_pub/main.cpp
@@ -13,13 +13,21 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
+  ros::Publisher pub_info = nh.advertise<sensor_msgs::CameraInfo>("camera/camera_info",1);
   cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  double  k[] = {1060, 0, image.cols/2,  0, 1060, image.cols/2,0,0 , 1};
   cv::waitKey(30);
   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+  sensor_msgs::CameraInfo msg_info = sensor_msgs::CameraInfo();
+  msg_info.height = image.rows;
+  msg_info.width = image.cols;
+  for (int i = 0 ; i < 9; i++)
+    msg_info.K[i] = k[i];
 
   ros::Rate loop_rate(1);
   while (nh.ok()) {
     pub.publish(msg);
+    pub_info.publish(msg_info);
     ros::spinOnce();
     loop_rate.sleep();
   }
